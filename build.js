d51ee25db54093bf7043c8d73363c9324ad2932d
Gautam Mittal <gautam@mittal.net>
2018-02-04 14:01:26 -0800

    Clean.

--- build.js
+++ build.js
@@ -101,4 +101,4 @@ let compile = (contentDir, outputDir, ignore) => {
 
 
 // Yesterday is history, tomorrow is a mystery, but today is a gift.
-compile(`content`, `thoughts`, `.ignore`);
+compile(`content`, `profiles`, `.ignore`);
