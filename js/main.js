a632203f316e043b413f656a40552430bdb67080
Gautam Mittal <gautam@mittal.net>
2018-02-13 19:05:03 -0800

    Add days left until graduation.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-function initTwitter() {
-	$(".twitter-timeline").height($("#pictureOfMe").height()+20);
+const graduation = moment(new Date("May 31, 2018"));
+const today = moment();
+const daysLeft = graduation.diff(today, 'days');
+let msg = `${daysLeft} days left. Make them count`;
+if (daysLeft == 0) {
+  msg = `Today we graduate. Let's make it count.`;
+} else if (daysLeft < 0) {
+  msg = `Hosted by Thomas Burton and Kevin Huang.`;
 }
 
-$(window).resize(function() {
-	$(".twitter-timeline").height($("#pictureOfMe").height()+20);
-});
+document.querySelector('.daysLeft').innerHTML = msg;
