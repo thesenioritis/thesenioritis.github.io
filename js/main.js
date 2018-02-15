6295a23148b374fb23a1254f96c8812d2af10b07
Gautam Mittal <gautam@mittal.net>
2018-02-14 22:30:34 -0800

    Update day counter fix.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const graduation = moment(new Date("May 31, 2018"));
 const today = moment();
-const daysLeft = graduation.diff(today, 'days');
+const daysLeft = graduation.diff(today, 'days')+1;
 let msg = `${daysLeft} days left. Make them count`;
 if (daysLeft == 0) {
   msg = `Today we graduate. Let's make it count.`;
