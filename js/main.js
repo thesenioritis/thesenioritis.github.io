781213304eeb040bad8332455f4223e76e326c17
Gautam Mittal <gautam@mittal.net>
2018-02-15 16:50:11 -0800

    Update counter.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const graduation = moment(new Date("May 31, 2018"));
 const today = moment();
 const daysLeft = graduation.diff(today, 'days')+1;
-let msg = `${daysLeft} days left. Make them count`;
+let msg = `${daysLeft} days left. Make them count.`;
 if (daysLeft == 0) {
   msg = `Today we graduate. Let's make it count.`;
 } else if (daysLeft < 0) {
